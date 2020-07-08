470be247bc8b7545fa7a9827ace33aaff1a136c5
Anders F Björklund <anders.f.bjorklund@gmail.com>
2020-07-08 20:19:25 +0200

    Add package for cni-plugins 0.8.6
    (cherry picked from commit 2c90a12ef932fc59c5903a363cd1d0e0dfc7ec98)

---
+++ external/package/cni-plugins/cni-plugins.mk
@@ -0,0 +1,34 @@
+################################################################################
+#
+# cni-plugins
+#
+################################################################################
+
+CNI_PLUGINS_VERSION = 0.8.6
+CNI_PLUGINS_SITE = https://github.com/containernetworking/plugins/archive
+CNI_PLUGINS_LICENSE = Apache-2.0
+
+CNI_PLUGINS_SOURCE = v$(CNI_PLUGINS_VERSION).tar.gz
+
+CNI_PLUGINS_MAKE_ENV = \
+	CGO_ENABLED=0 \
+	GO111MODULE=on
+
+CNI_PLUGINS_BUILDFLAGS = -ldflags '-extldflags -static -X github.com/containernetworking/plugins/pkg/utils/buildversion.BuildVersion=v$(CNI_PLUGINS_VERSION)'
+
+define CNI_PLUGINS_BUILD_CMDS
+	(cd $(@D); $(CNI_PLUGINS_MAKE_ENV) ./build_linux.sh $(CNI_PLUGINS_BUILDFLAGS))
+endef
+
+define CNI_PLUGINS_INSTALL_TARGET_CMDS
+	cd $(@D); for bin in bin/*; do \
+		$(INSTALL) -D -m 0755 \
+			$(@D)/$$bin \
+			$(TARGET_DIR)/opt/cni/$$bin; \
+		ln -sf \
+			../../opt/cni/$$bin \
+			$(TARGET_DIR)/usr/$$bin; \
+	done
+endef
+
+$(eval $(generic-package))

049be0454aa4dccd1214a9a91c45ece3f939d2eb
Anders F Björklund <anders.f.bjorklund@gmail.com>
2020-07-08 20:18:34 +0200

    Add package for cri-tools 1.18.0
    (cherry picked from commit e48d182d47abee79f54fb92e07803c0c2143346c)

---
+++ external/package/cri-tools/cri-tools.mk
@@ -0,0 +1,29 @@
+################################################################################
+#
+# cri-tools
+#
+################################################################################
+
+CRI_TOOLS_VERSION = 1.18.0
+CRI_TOOLS_SITE = https://github.com/kubernetes-sigs/cri-tools/archive
+CRI_TOOLS_LICENSE = Apache-2.0
+
+CRI_TOOLS_SOURCE = v$(CRI_TOOLS_VERSION).tar.gz
+
+ifeq ($(BR2_PACKAGE_CRI_TOOLS_CRICTL),y)
+	CRI_TOOLS_PROGRAMS += crictl
+endif
+ifeq ($(BR2_PACKAGE_CRI_TOOLS_CRITEST),y)
+	CRI_TOOLS_PROGRAMS += critest
+endif
+
+define CRI_TOOLS_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) VERSION=v$(CRI_TOOLS_VERSION) $(CRI_TOOLS_PROGRAMS)
+endef
+define CRI_TOOLS_INSTALL_TARGET_CMDS
+	for program in $(CRI_TOOLS_PROGRAMS); do \
+		$(INSTALL) -D -m 0755 $(@D)/_output/$$program $(TARGET_DIR)/usr/bin; \
+	done
+endef
+
+$(eval $(generic-package))

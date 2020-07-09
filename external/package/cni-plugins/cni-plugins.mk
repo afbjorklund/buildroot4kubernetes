75c6378a33aced9d81ae99ca364996c17135ff09
Anders F Björklund <anders.f.bjorklund@gmail.com>
2020-07-09 18:37:35 +0200

    Split cni-plugins programs into basic and extra
    Somewhat arbitrary, to cut down on install size
    
    (cherry picked from commit a9f8a87e3dfc1cdc34e168b4f4e68601c0e897c7)

--- external/package/cni-plugins/cni-plugins.mk
+++ external/package/cni-plugins/cni-plugins.mk
@@ -17,12 +17,23 @@ CNI_PLUGINS_MAKE_ENV = \
 
 CNI_PLUGINS_BUILDFLAGS = -ldflags '-extldflags -static -X github.com/containernetworking/plugins/pkg/utils/buildversion.BuildVersion=v$(CNI_PLUGINS_VERSION)'
 
+ifeq ($(BR2_PACKAGE_CNI_PLUGINS_BASIC),y)
+	CNI_PLUGINS_PROGRAMS += bridge loopback ptp
+	CNI_PLUGINS_PROGRAMS += flannel portmap sbr
+endif
+ifeq ($(BR2_PACKAGE_CNI_PLUGINS_EXTRA),y)
+	CNI_PLUGINS_PROGRAMS += ipvlan macvlan vlan host-device
+	CNI_PLUGINS_PROGRAMS += dhcp host-local static
+	CNI_PLUGINS_PROGRAMS += tuning bandwidth firewall
+endif
+
 define CNI_PLUGINS_BUILD_CMDS
 	(cd $(@D); $(CNI_PLUGINS_MAKE_ENV) ./build_linux.sh $(CNI_PLUGINS_BUILDFLAGS))
 endef
 
 define CNI_PLUGINS_INSTALL_TARGET_CMDS
-	cd $(@D); for bin in bin/*; do \
+	cd $(@D); for program in $(CNI_PLUGINS_PROGRAMS); do \
+		bin=bin/$$program; \
 		$(INSTALL) -D -m 0755 \
 			$(@D)/$$bin \
 			$(TARGET_DIR)/opt/cni/$$bin; \

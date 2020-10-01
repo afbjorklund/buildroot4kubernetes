24c02121c7b68c8e24d0f7a4e13811adc6da4f45
Anders F Björklund <anders.f.bjorklund@gmail.com>
2020-10-01 22:43:26 +0200

    Add no pivot_root setting for docker on rootfs

--- external/post_build.sh
+++ external/post_build.sh
@@ -15,3 +15,7 @@ ln -sf /dev/null $TARGET_DIR/etc/systemd/network/99-default.link
 mkdir -p $TARGET_DIR/etc/systemd/system/getty.target.wants
 ln -sf /lib/systemd/system/getty@.service $TARGET_DIR/etc/systemd/system/getty.target.wants/getty@tty1.service
 
+# can't use pivot_root when running on rootfs, only when root is on e.g. tmpfs
+
+mkdir -p $TARGET_DIR/etc/default
+echo "export DOCKER_RAMDISK=true" > $TARGET_DIR/etc/default/dockerd

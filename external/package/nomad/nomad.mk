52f85f15f8e130705d4faf11a7b37899447a4791
Anders F Björklund <anders.f.bjorklund@gmail.com>
2020-11-01 20:59:49 +0100

    Add packaging for hashicorp nomad orchestrator
    Removing support for cloud providers cuts binary size in half!
    
    The go mod dependencies for all those cloud sdks are quite big.

---
+++ external/package/nomad/nomad.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# nomad
+#
+################################################################################
+
+NOMAD_VERSION = 0.11.6
+NOMAD_SITE = $(call github,hashicorp,nomad,v$(NOMAD_VERSION))
+NOMAD_LICENSE = MPL-2.0
+NOMAD_LICENSE_FILES = LICENSE
+
+NOMAD_WORKSPACE = vendor
+
+NOMAD_LDFLAGS = \
+	-X github.com/hashicorp/nomad/version.GitCommit=$(NOMAD_VERSION)
+
+NOMAD_TAGS = codegen_generated
+
+NOMAD_INSTALL_BINS = nomad
+
+$(eval $(golang-package))

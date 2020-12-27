2e0e300bcb556367291540b7e79ca3ea0720df51
Anders F Björklund <anders.f.bjorklund@gmail.com>
2020-12-27 14:46:30 +0100

    Update kubepkg version without changes to kubelet

--- external/package/kubernetes-kubelet/kubernetes-kubelet.mk
+++ external/package/kubernetes-kubelet/kubernetes-kubelet.mk
@@ -12,7 +12,7 @@ KUBERNETES_KUBELET_LICENSE = Apache-2.0
 KUBERNETES_KUBELET_SOURCE = $(KUBERNETES_SOURCE)
 
 # See https://github.com/kubernetes/release
-KUBERNETES_KUBEPKG_RELEASE_VERSION = v0.3.2
+KUBERNETES_KUBEPKG_RELEASE_VERSION = v0.6.0
 
 define KUBERNETES_KUBELET_BUILD_CMDS
 	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) generated_files
@@ -28,8 +28,8 @@ define KUBERNETES_KUBELET_INSTALL_INIT_SYSTEMD
 	$(INSTALL) -D -m 0644 $(KUBERNETES_KUBELET_PKGDIR)/kubelet.service \
 		$(TARGET_DIR)/usr/lib/systemd/system/kubelet.service
 
-	# "https://raw.githubusercontent.com/kubernetes/release/$(KUBERNETES_KUBEPKG_RELEASE_VERSION)/cmd/kubepkg/templates/latest/deb/KUBELET/10-KUBELET.conf"
-	$(INSTALL) -D -m 0644 $(KUBERNETES_KUBELET_PKGDIR)/kubelet.service.d/10-kubeadm.conf \
+	# "https://raw.githubusercontent.com/kubernetes/release/$(KUBERNETES_KUBEPKG_RELEASE_VERSION)/cmd/kubepkg/templates/latest/deb/kubeadm/10-kubeadm.conf"
+	$(INSTALL) -D -m 0644 $(KUBERNETES_KUBELET_PKGDIR)/10-kubeadm.conf \
 		$(TARGET_DIR)/usr/lib/systemd/system/kubelet.service.d/10-kubeadm.conf
 endef
 

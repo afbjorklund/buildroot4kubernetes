5266451eda3d431e3f4d699ea2f2b36e45cbb0fa
Anders F Björklund <anders.f.bjorklund@gmail.com>
2021-12-27 13:17:47 +0100

    Upgrade kubernetes

--- external/package/kubernetes/kubernetes.mk
+++ external/package/kubernetes/kubernetes.mk
@@ -4,8 +4,9 @@
 #
 ################################################################################
 
-KUBERNETES_VERSION = 1.21.0
+KUBERNETES_VERSION = 1.23.0
 KUBERNETES_SITE = $(call github,kubernetes,kubernetes,v$(KUBERNETES_VERSION))
 KUBERNETES_LICENSE = Apache-2.0
+KUBERNETES_LICENSE_FILES = LICENSE
 
 $(eval $(generic-package))

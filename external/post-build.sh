87f932d84d0192c56a01a59e00d8d71f988f7f49
Anders F Björklund <anders.f.bjorklund@gmail.com>
2022-03-20 09:33:13 +0100

    Upgrade buildroot

--- external/post-build.sh
+++ external/post-build.sh
@@ -8,7 +8,7 @@ BOARD_DIR=$(dirname "$0")
 cp -f "$BOARD_DIR/grub-bios.cfg" "$TARGET_DIR/boot/grub/grub.cfg"
 
 # Copy grub 1st stage to binaries, required for genimage
-cp -f "$HOST_DIR/lib/grub/i386-pc/boot.img" "$BINARIES_DIR"
+cp -f "$TARGET_DIR/lib/grub/i386-pc/boot.img" "$BINARIES_DIR"
 
 # https://www.freedesktop.org/wiki/Software/systemd/PredictableNetworkInterfaceNames/
 

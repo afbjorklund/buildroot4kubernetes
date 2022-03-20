f37432d30f24e7a4aa3be4751a5cef9319b20add
Anders F Björklund <anders.f.bjorklund@gmail.com>
2022-03-20 10:14:59 +0100

    Upgrade cri-tools

--- external/package/cri-tools/cri-tools.mk
+++ external/package/cri-tools/cri-tools.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CRI_TOOLS_VERSION = 1.21.0
+CRI_TOOLS_VERSION = 1.23.0
 CRI_TOOLS_SITE = $(call github,kubernetes-sigs,cri-tools,v$(CRI_TOOLS_VERSION))
 CRI_TOOLS_LICENSE = Apache-2.0
 CRI_TOOLS_LICENSE_FILES = LICENSE

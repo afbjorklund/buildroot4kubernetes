caaedfdb3f504fdf94650f1c363f60e661c403e8
Anders F Björklund <anders.f.bjorklund@gmail.com>
2022-05-05 20:46:00 +0200

    Upgrade cri-tools to 1.24.0
    Make a proper buildroot package for the cri tools,
    change to the golang type and do proper mod vendoring.

--- external/package/cri-tools/cri-tools.mk
+++ external/package/cri-tools/cri-tools.mk
@@ -4,29 +4,23 @@
 #
 ################################################################################
 
-CRI_TOOLS_VERSION = 1.23.0
+CRI_TOOLS_VERSION = 1.24.0
 CRI_TOOLS_SITE = $(call github,kubernetes-sigs,cri-tools,v$(CRI_TOOLS_VERSION))
 CRI_TOOLS_LICENSE = Apache-2.0
 CRI_TOOLS_LICENSE_FILES = LICENSE
 
-CRI_TOOLS_MAKE_ENV = \
-	$(GO_TARGET_ENV) \
-	GO111MODULE=on
+CRI_TOOLS_GOMOD = github.com/kubernetes-sigs/cri-tools
+
+CRI_TOOLS_LDFLAGS = \
+	-X $(CRI_TOOLS_GOMOD)/pkg/version.Version=$(CRI_TOOLS_VERSION)
 
 ifeq ($(BR2_PACKAGE_CRI_TOOLS_CRICTL),y)
-	CRI_TOOLS_PROGRAMS += crictl
+	CRI_TOOLS_BUILD_TARGETS += cmd/crictl
 endif
 ifeq ($(BR2_PACKAGE_CRI_TOOLS_CRITEST),y)
-	CRI_TOOLS_PROGRAMS += critest
+	CRI_TOOLS_BUILD_TARGETS += cmd/critest
 endif
 
-define CRI_TOOLS_BUILD_CMDS
-	$(CRI_TOOLS_MAKE_ENV) $(MAKE) -C $(@D) VERSION=v$(CRI_TOOLS_VERSION) $(CRI_TOOLS_PROGRAMS)
-endef
-define CRI_TOOLS_INSTALL_TARGET_CMDS
-	for program in $(CRI_TOOLS_PROGRAMS); do \
-		$(INSTALL) -D -m 0755 $(@D)/build/bin/$$program $(TARGET_DIR)/usr/bin; \
-	done
-endef
+CRI_TOOLS_INSTALL_BINS = $(notdir $(CRI_TOOLS_BUILD_TARGETS))
 
-$(eval $(generic-package))
+$(eval $(golang-package))

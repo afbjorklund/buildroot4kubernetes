08617656bbed10181a85a765085f817f125f0656
Anders F Björklund <anders.f.bjorklund@gmail.com>
2022-05-08 12:59:11 +0200

    Add arch and board support for aarch64 raspberrypi

---
+++ external/board/kubernetes/aarch64/post-build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -u
+set -e
+
+BOARD_DIR=$(dirname "$0")
+
+# https://www.freedesktop.org/wiki/Software/systemd/PredictableNetworkInterfaceNames/
+
+mkdir -p $TARGET_DIR/etc/systemd/network
+ln -sf /dev/null $TARGET_DIR/etc/systemd/network/99-default.link
+
+# https://www.freedesktop.org/software/systemd/man/systemd-getty-generator.html
+
+mkdir -p $TARGET_DIR/etc/systemd/system/getty.target.wants
+ln -sf /lib/systemd/system/getty@.service $TARGET_DIR/etc/systemd/system/getty.target.wants/getty@tty1.service
+
+mkdir -p $TARGET_DIR/etc/systemd/system/getty@.service.d
+printf "[Service]\nTTYVTDisallocate=no\n" > $TARGET_DIR/etc/systemd/system/getty@.service.d/noclear.conf
+
+# https://kubernetes.io/docs/setup/production-environment/container-runtimes/#docker
+
+mkdir -p $TARGET_DIR/etc/docker
+cp $BOARD_DIR/daemon.json $TARGET_DIR/etc/docker
+
+mkdir -p $TARGET_DIR/etc/systemd/system/docker.service.d

0af5ac556a195caa4d38bb9321ee01f456c31da0
Anders F Björklund <anders.f.bjorklund@gmail.com>
2022-05-08 22:01:22 +0200

    The genimage config no longer matches board name

--- external/board/kubernetes/aarch64/post-image.sh
+++ external/board/kubernetes/aarch64/post-image.sh
@@ -4,7 +4,7 @@ set -e
 
 BOARD_DIR="$(dirname $0)"
 BOARD_NAME="$(basename ${BOARD_DIR})"
-GENIMAGE_CFG="${BOARD_DIR}/genimage-${BOARD_NAME}.cfg"
+GENIMAGE_CFG="${BOARD_DIR}/genimage-raspberrypi3-64.cfg"
 GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
 
 # Pass an empty rootpath. genimage makes a full copy of the given rootpath to

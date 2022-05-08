b0b444838f570edb03eb4c6187172477ec5b2834
Anders F Björklund <anders.f.bjorklund@gmail.com>
2022-05-08 18:31:41 +0200

    Add workaround for hardcoded cri-dockerd targets

--- external/package/cri-dockerd/cri-dockerd.mk
+++ external/package/cri-dockerd/cri-dockerd.mk
@@ -20,6 +20,7 @@ CRI_DOCKERD_ENV = \
 	GOBIN="$(CRI_DOCKERD_GOPATH)/bin" \
 	PATH=$(CRI_DOCKERD_GOPATH)/bin:$(BR_PATH)
 
+ifeq ($(BR2_x86_64),y)
 define CRI_DOCKERD_BUILD_CMDS
 	$(CRI_DOCKERD_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) static-linux
 endef
@@ -29,6 +30,19 @@ define CRI_DOCKERD_INSTALL_TARGET_CMDS
 		$(@D)/packaging/static/build/linux/cri-dockerd/cri-dockerd \
 		$(TARGET_DIR)/usr/bin/cri-dockerd
 endef
+endif
+ifeq ($(BR2_aarch64),y)
+define CRI_DOCKERD_BUILD_CMDS
+	# $(CRI_DOCKERD_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) cross-arm
+	$(CRI_DOCKERD_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/packaging/static APP_DIR=$(@D)/src GO_VERSION=$(GO_VERSION) cross-arm
+endef
+
+define CRI_DOCKERD_INSTALL_TARGET_CMDS
+	$(INSTALL) -Dm755 \
+		$(@D)/packaging/static/build/arm/cri-dockerd/cri-dockerd \
+		$(TARGET_DIR)/usr/bin/cri-dockerd
+endef
+endif
 
 define CRI_DOCKERD_INSTALL_INIT_SYSTEMD
 	$(INSTALL) -Dm644 \

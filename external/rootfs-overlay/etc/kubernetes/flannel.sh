f0ce466d301ec3597c61a038fbb75e49d5266c76
Anders F Björklund <anders.f.bjorklund@gmail.com>
2022-12-21 18:28:48 +0100

    Add the explicit docker cri-socket adress

--- external/rootfs-overlay/etc/kubernetes/flannel.sh
+++ external/rootfs-overlay/etc/kubernetes/flannel.sh
@@ -1,5 +1,6 @@
 kubeadm init --pod-network-cidr=10.244.0.0/16 \
              --apiserver-cert-extra-sans=127.0.0.1 \
+             --cri-socket=unix://var/run/cri-dockerd.sock \
              --kubernetes-version=v1.25.0
 
 export KUBECONFIG=/etc/kubernetes/admin.conf
